48c99836c342c0cc16b7437b8b6f5c383d564cc9
root <Jerry M>
2022-03-22 01:34:14 +0000

    it begins

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+
+<script>
+	let sentence = '';
+	const nospchar = /[^A-Za-z\s]+/;
+	const dashes = /-/;
+	import {bible} from './wordlist.json';
+	
+	let slength = 0;
+	let words_in_bible = 0;
+	let fsentence = null;
+	
+	function a(){
+		words_in_bible = 0;
+		fsentence = sentence.toLowerCase().replace(dashes, ' ').replace(nospchar, '').split(' ');
+		slength = fsentence.length;
+		for(let i = 0; i < slength; i++){
+			if(bible.includes(fsentence[i])){
+				words_in_bible++;
+			}
+		}
+	}
+	
+</script>
+
+<center><h1>"None of those words are in the Bible."</h1></center>
+
+<center><input bind:value={sentence} on:input={e => a()}></center>
+<br>
+<center>Your sentence has {slength} {slength == 1 ? "word" : "words"}. {words_in_bible == 0 ? "None" : words_in_bible} of them {words_in_bible == 1 ? "is" : "are"} in the Bible.</center>
+<center>Your sentence is {slength == 0 ? "???" : words_in_bible/slength*100}% holy.</center>
+<br>
+<br>
+<br>
+<center>Made with &#128405; by Jeremy Mausman</center>
